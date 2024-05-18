3ea9798147f53ac0872cde1e4083a89581da78fb
diobrendoo <bankay518@gmail.com>
2024-05-18 20:41:43 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -18,8 +18,8 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let _uptime = process.uptime() * 1000
     let uptime = clockString(_uptime)
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-    let videoUrl = 'https://telegra.ph/file/37649d24d8de19dc61fe6.mp4';
+    if (!(who in global.db.data.users)) throw `✳️ المستخدم غير موجود في قاعدة البيانات الخاصة بي`
+    let videoUrl = 'https://telegra.ph/file/28d5346f49883f1cf9ed1.mp4';
     let vn = '.Menu.png';
     const user = global.db.data.users[m.sender];
     const {money, joincount} = global.db.data.users[m.sender];
