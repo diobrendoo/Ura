51ef146dc3dd0efc517ceec5d48a8c22121b8b25
diobrendoo <bankay518@gmail.com>
2024-06-06 18:57:42 +0300

    Update رونالدو.js

--- plugins/رونالدو.js
+++ plugins/رونالدو.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   const ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())];
   conn.sendFile(m.chat, ronaldo, 'error.jpg', `*Siiiuuuuuu*`, m);
 };
- //conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
+conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
 handler.help = ['cristianoronaldo', 'cr7'];
 handler.tags = ['internet'];
 handler.command = /^(رونالدو|كريستيانو)$/i;
