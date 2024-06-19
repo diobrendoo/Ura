fce01044e373309e81f4504004a6e647ac194706
diobrendoo <bankay518@gmail.com>
2024-06-19 22:34:34 +0300

    Update رد.js

--- plugins/رد.js
+++ plugins/رد.js
@@ -2,9 +2,8 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `بوت ماكيما تحت امرك لتلقي الاوامر اكتب(.اوامر)`;
 
-    conn.sendFile(m.chat, 'https://telegra.ph/file/b574a4b50331200691bfa.jpg', 'image.jpg', message, m);
+    conn.sendFile(m.chat, 'https://telegra.ph/file/b574a4b50331200691bfa.jpg', 'sticker', m);
 };
 
 handler.customPrefix = /^ماكيما$/i;
